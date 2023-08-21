5001ef802b37031baf259e1a939d4760dc7a18f2
Harsh Surti <surtiharsh1212@gmail.com>
2023-08-21 23:27:31 +0530

    module based planning

--- Back-end/authenticate.php
+++ Back-end/authenticate.php
@@ -1,24 +1,27 @@
 <?php
 require '../Back-end/dbconnect.php';
 
-if (["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lmail = $_POST['lmail'];
     $lpswd = $_POST['lpswd'];
 
-    $query = "SELECT pswd FROM student WHERE semail = `$lmail`";
-    $result = $conn->query($query);
+    $student_query = "SELECT * FROM student WHERE semail = '$lmail' and pswd = '$lpswd'";
+    $faculty_query = "SELECT * FROM faculty WHERE femail = '$lmail' and fpswd = '$lpswd'";
 
-    if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-
-        if (password_verify($lpswd, $row['pswd'])) {
-            echo "Login successfully with <br>";
-            echo "SELECT pswd FROM student WHERE semail = `$lmail`";
-        } else {
-            echo "Invalid password";
-        }
-    } else {
-        echo "User not found";
+    $student_result = mysqli_query($conn,$student_query);
+    $faculty_result = mysqli_query($conn,$faculty_query);
+    
+    if ($student_result->num_rows == 1) {
+        // $home = "";
+        header("Location : ../Front-end/index.html");
+        
     }
+    elseif ($faculty_result->num_rows == 1) {
+        echo "Login successfully as student";
+    }else{
+        $login = "../Front-end/login.html";
+        header("Location: ". $login);
+        echo "Not found account sign up first";
+    }  
 }
 ?>
