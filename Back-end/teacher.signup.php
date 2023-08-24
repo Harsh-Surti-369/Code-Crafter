0475a1ee8188cf9d248bbe65335bdbec04ae2e2f
Harsh Surti <surtiharsh1212@gmail.com>
2023-08-24 23:07:59 +0530

    Explore cousre page design

--- Back-end/teacher.signup.php
+++ Back-end/teacher.signup.php
@@ -55,6 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- bootstrap css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+
     <!-- jquery -->
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <!-- bootstrap js -->
